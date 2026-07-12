4f3d63c92484fc9c0b392610bd34bc0f7959f58f
sarthakrajput17 <rajput17sarthak@gmail.com>
2026-07-12 18:21:52 +0530

    Day 3 - Added GET user endpoints

--- app/src/db.py
+++ app/src/db.py
@@ -10,3 +10,80 @@ def get_db_connection():
         password=Config.DB_PASSWORD
     )
     return conn
+
+def create_user(name, email):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO users (name, email)
+        VALUES (%s, %s)
+        RETURNING id;
+        """,
+        (name, email)
+    )
+
+    user_id = cursor.fetchone()[0]
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return user_id
+
+def get_all_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, name, email, created_at
+        FROM users
+        ORDER BY id;
+    """)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    users = []
+
+    for row in rows:
+        users.append({
+            "id": row[0],
+            "name": row[1],
+            "email": row[2],
+            "created_at": str(row[3])
+        })
+
+    return users
+
+def get_user_by_id(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT id, name, email, created_at
+        FROM users
+        WHERE id = %s;
+        """,
+        (user_id,)
+    )
+
+    row = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if row is None:
+        return None
+
+    return {
+        "id": row[0],
+        "name": row[1],
+        "email": row[2],
+        "created_at": str(row[3])
+    }
--- app/src/routes.py
+++ app/src/routes.py
@@ -1,20 +1,64 @@
-from flask import jsonify
-from db import get_db_connection
+from flask import request, jsonify
+from db import (
+    get_db_connection,
+    create_user,
+    get_all_users,
+    get_user_by_id
+)
+
 
 def register_routes(app):
 
     @app.route("/health", methods=["GET"])
     def health():
+
         try:
             conn = get_db_connection()
             conn.close()
+
             return jsonify({
                 "status": "ok",
                 "database": "connected"
-            }), 200
+            })
+
         except Exception as e:
             return jsonify({
                 "status": "error",
-                "database": "disconnected",
                 "message": str(e)
             }), 500
+
+
+    @app.route("/users", methods=["POST"])
+    def add_user():
+
+        data = request.get_json()
+
+        name = data["name"]
+        email = data["email"]
+
+        user_id = create_user(name, email)
+
+        return jsonify({
+            "message": "User created successfully",
+            "id": user_id
+        }), 201
+    
+    @app.route("/users", methods=["GET"])
+    def get_users():
+
+        users = get_all_users()
+
+        return jsonify(users), 200
+    
+
+    @app.route("/users/<int:user_id>", methods=["GET"])
+    def get_user(user_id):
+
+        user = get_user_by_id(user_id)
+
+        if user is None:
+            return jsonify({
+                "message": "User not found"
+            }), 404
+
+        return jsonify(user), 200
